980df626940d6d8822eed44cfbb10cc1879fad3e
Duncan558 <dwachira2002@gmail.com>
2025-11-18 09:49:32 +0300

    Complete temperature conversion lab: totals and averages

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-//! Start by creating the variables for the data recorded
-//* Then work on the conversion of the temperature from Celsius to Fahrenheit (or viceversa)
+const temperaturesInCelsius = [12, 14, 9, 20, 18, 16, 11];
 
+function cToF(c) {
+  return (c * 9 / 5) + 32;
+}
 
-//! Start the calculation of the total temperatures
-//* Then apply the conversion to calculate the total in the other unit of measurement
-//* Call the variables: tot_temperature_in_fahrenheit and tot_temperature_in_celsius
+const temperaturesInFahrenheit = temperaturesInCelsius.map(cToF);
 
-//! Start the calculation of the average temperatures
-//* Call the variables: avg_temperature_in_fahrenheit and avg_temperature_in_celsius
+const tot_temperature_in_celsius = temperaturesInCelsius.reduce((sum, v) => sum + v, 0);
+const tot_temperature_in_fahrenheit = temperaturesInFahrenheit.reduce((sum, v) => sum + v, 0);
 
-//! Console.log the results for your own inspection if you'd like
+const n = temperaturesInCelsius.length;
 
-//! After creating the four variables mentioned above, uncomment the following lines
-//* This way you can export them to the test file, this is essential for the tests to work
+const avg_temperature_in_celsius = Number((tot_temperature_in_celsius / n).toFixed(2));
+const avg_temperature_in_fahrenheit = Number((tot_temperature_in_fahrenheit / n).toFixed(2));
 
 module.exports = {
-    // tot_temperature_in_fahrenheit,
-    // tot_temperature_in_celsius,
-    // avg_temperature_in_fahrenheit,
-    // avg_temperature_in_celsius
+    tot_temperature_in_fahrenheit,
+    tot_temperature_in_celsius,
+    avg_temperature_in_fahrenheit,
+    avg_temperature_in_celsius
 };
+
